52953b0d46e9b73e07f2795ce1a5fe8249cfc28c
Yves Van den Meerssche <yves@livethelife.tv>
2020-04-01 16:22:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Progressive-Web-Application
+PWA of LTL Maps. (React/AWS S3/ZEIT). Fixing unreliable reviews. Five-star systems are good at identifying and weeding out very low-quality places, they do a poor job of separating good from great. Who has a similar lifestyle and worldview? Get trustworthy recommendations from your friends and add their favorite places to your own map. 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/b77f6952-d9ed-4808-882b-1f6d810a99c3/deploy-status)](https://app.netlify.com/sites/ltl/deploys)
---
+++ build/precache-manifest.8e89a877510debd56f54571b0ef024e3.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89af5e8c8b42a6af8803dc0505da5fb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa49fefaef7d6e21950c",
+    "url": "/static/css/1.fdcc8277.chunk.css"
+  },
+  {
+    "revision": "34c9052d81da5902ed99",
+    "url": "/static/css/2.e0db884e.chunk.css"
+  },
+  {
+    "revision": "8c7ad8e4031bbf89ddb6",
+    "url": "/static/css/20.a4ac7d33.chunk.css"
+  },
+  {
+    "revision": "65ab20d08613d109be29",
+    "url": "/static/css/21.c0725b88.chunk.css"
+  },
+  {
+    "revision": "428dd1958c1315d7f738",
+    "url": "/static/css/4.8868fb89.chunk.css"
+  },
+  {
+    "revision": "173b8afdd93089e35699",
+    "url": "/static/css/AgentAccountSettingsPage.1472f44e.chunk.css"
+  },
+  {
+    "revision": "25611ecde0e6d4c9cb07",
+    "url": "/static/css/AgentDetailsViewPage.3db07d74.chunk.css"
+  },
+  {
+    "revision": "b2f713194a41267d576c",
+    "url": "/static/css/Listing.92b64754.chunk.css"
+  },
+  {
+    "revision": "8dcd5ecb3f7b6c3ba816",
+    "url": "/static/css/Pricing.0ef9204e.chunk.css"
+  },
+  {
+    "revision": "d838e48796ddfd7e83a4",
+    "url": "/static/css/RenderListingForm.14ba606c.chunk.css"
+  },
+  {
+    "revision": "512ab19f00ae8d48a595",
+    "url": "/static/css/SignIn.7eaa8044.chunk.css"
+  },
+  {
+    "revision": "84efd2efffdc42484f36",
+    "url": "/static/css/SignUp.7eaa8044.chunk.css"
+  },
+  {
+    "revision": "f1e8a91c0640b7311722",
+    "url": "/static/css/SinglePageView.0940fdcd.chunk.css"
+  },
+  {
+    "revision": "366db2ffd6ad2a893fe3",
+    "url": "/static/js/0.0a679f92.chunk.js"
+  },
+  {
+    "revision": "fa49fefaef7d6e21950c",
+    "url": "/static/js/1.b92247b2.chunk.js"
+  },
+  {
+    "revision": "34c9052d81da5902ed99",
+    "url": "/static/js/2.da1f6d40.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/static/js/2.da1f6d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c7ad8e4031bbf89ddb6",
+    "url": "/static/js/20.2820e1e1.chunk.js"
+  },
+  {
+    "revision": "41b26c47bc0a6b3b11aa94eb3e5ca96a",
+    "url": "/static/js/20.2820e1e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65ab20d08613d109be29",
+    "url": "/static/js/21.ab94e9f0.chunk.js"
+  },
+  {
+    "revision": "efcca69e6881d499f081e5e8742c5eb8",
+    "url": "/static/js/21.ab94e9f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa2b0b455d1a0d3d1dd5",
+    "url": "/static/js/3.2503ef3e.chunk.js"
+  },
+  {
+    "revision": "428dd1958c1315d7f738",
+    "url": "/static/js/4.417fd551.chunk.js"
+  },
+  {
+    "revision": "50464c349cdc12dd6231",
+    "url": "/static/js/5.9de9a562.chunk.js"
+  },
+  {
+    "revision": "173b8afdd93089e35699",
+    "url": "/static/js/AgentAccountSettingsPage.7c2c3e3f.chunk.js"
+  },
+  {
+    "revision": "25611ecde0e6d4c9cb07",
+    "url": "/static/js/AgentDetailsViewPage.e95f199f.chunk.js"
+  },
+  {
+    "revision": "08eb5c2fe844e9dd8ef4",
+    "url": "/static/js/ForgetPassWord.ca1f0290.chunk.js"
+  },
+  {
+    "revision": "62f15f1ae1327bfc41cf",
+    "url": "/static/js/Home.a969a63f.chunk.js"
+  },
+  {
+    "revision": "b2f713194a41267d576c",
+    "url": "/static/js/Listing.0e8eac8b.chunk.js"
+  },
+  {
+    "revision": "d0936812c73c116e57f2",
+    "url": "/static/js/NotFound.bc2d0c74.chunk.js"
+  },
+  {
+    "revision": "8dcd5ecb3f7b6c3ba816",
+    "url": "/static/js/Pricing.7af3001d.chunk.js"
+  },
+  {
+    "revision": "d838e48796ddfd7e83a4",
+    "url": "/static/js/RenderListingForm.43d51d50.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/static/js/RenderListingForm.43d51d50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "512ab19f00ae8d48a595",
+    "url": "/static/js/SignIn.fc3a0c46.chunk.js"
+  },
+  {
+    "revision": "84efd2efffdc42484f36",
+    "url": "/static/js/SignUp.09f27a97.chunk.js"
+  },
+  {
+    "revision": "f1e8a91c0640b7311722",
+    "url": "/static/js/SinglePageView.0036b1a7.chunk.js"
+  },
+  {
+    "revision": "c307d458d54c9e1524984b0ae1f3e612",
+    "url": "/static/js/SinglePageView.0036b1a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ff9c4b44e2998cbd3dd",
+    "url": "/static/js/main.cfb8baba.chunk.js"
+  },
+  {
+    "revision": "874f7e9caf481561d321",
+    "url": "/static/js/privacy.56fe4d78.chunk.js"
+  },
+  {
+    "revision": "364f910b05e09826975b",
+    "url": "/static/js/runtime-main.1275bbba.js"
+  },
+  {
+    "revision": "b39f523dbc8a6ab9b8bbce9a30f9c07f",
+    "url": "/static/media/1.b39f523d.jpg"
+  },
+  {
+    "revision": "fefe1080287d5aa0cbaf2136df440867",
+    "url": "/static/media/2.fefe1080.jpg"
+  },
+  {
+    "revision": "e8d9690cb08e021e3cd0c9c589b9de43",
+    "url": "/static/media/3.e8d9690c.jpg"
+  },
+  {
+    "revision": "6f0e20ab98e7f6011e0c0ddd69794fd1",
+    "url": "/static/media/404@2x.6f0e20ab.png"
+  },
+  {
+    "revision": "ca99d53638e7c2b199eed34f3765ce08",
+    "url": "/static/media/footer-bg.ca99d536.svg"
+  },
+  {
+    "revision": "ddd0f143e773e452e0ea9461c1cde37a",
+    "url": "/static/media/login-page-bg.ddd0f143.jpg"
+  },
+  {
+    "revision": "980da4299092b1415ed48ec836467e29",
+    "url": "/static/media/logo-alt.980da429.svg"
+  },
+  {
+    "revision": "5e663a8ff2ea89cb4c530f98d1722427",
+    "url": "/static/media/post-image-1.5e663a8f.jpg"
+  },
+  {
+    "revision": "e451ba2c5bce5a6cf6819ad7cf65fb70",
+    "url": "/static/media/post-image-2.e451ba2c.jpg"
+  },
+  {
+    "revision": "3a9fe4b0a58f9f7a6c469fadd3df5b54",
+    "url": "/static/media/post-thumb-1.3a9fe4b0.jpg"
+  },
+  {
+    "revision": "03cf1a5099604a6967adb01cdd10c94e",
+    "url": "/static/media/post-thumb-2.03cf1a50.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "a6c6afc8dff6391ec353accab363740f",
+    "url": "/static/media/single-post-bg.a6c6afc8.jpg"
+  }
+]);
